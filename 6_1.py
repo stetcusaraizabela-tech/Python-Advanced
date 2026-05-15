7ebe5d19015779ddecbde5eab451a1f0c5d0b304
JA-FR-O-09-23-1 <stetcusaraizabela@gmail.com>
2026-05-15 19:54:47 +0200

    intro to exeptions and try/exept

---
+++ 6_1.py
@@ -0,0 +1,56 @@
+#exception
+
+print(x)
+
+
+
+try:
+    pass
+except:
+    pass
+
+try:
+    print("a")
+    print(error)
+    print("Noo erroer")
+except:
+    print("erroer")
+
+print("Code after try/except fölkjkhg")
+
+try:
+    number = int("Hallo")
+except ValueError:
+    print("Hallo kajnn nihvghr striung in integer umwandelvm,nvfgjivuwghruzg8u8tuz,"
+          ",")
+except ZeroDivisionError:
+    print("darfnivihtdurchnull0divaiovjuihvrwjugwtvrgbq0000")
+
+
+try
+    value = int(input("Zahl pls"))
+except (ValueError, TypeError):
+    print("Eingabefehler gy<zxgf  jdm,fgj m"
+          "sdhefzwezazrweag"
+
+else:
+    print(f"das 2fache von {number} ist0{ number *2} ")
+
+try:
+    print("st")
+    x =1/0
+except ZeroDivisionError:
+    print("feheler,mkodepölölä")
+finally:
+    print("Diersef block läfugftfffffrffffffffffft     immer!")
+
+print("\n\n\nKomplettes bsp mit allBlöcken")
+try:
+    text = input("zahxl pslölpppppppppppppplssssssss")
+    number =int(text)
+except ValueError:
+    print("no zahl")
+else:
+    print("yayay mgga´´e´gescjafftummwandlung erfolgreich")
+finally:
+    print("beendet.pls töte mich ich biite dich ich kann nicht mehr. einführung in try/except beendet")
